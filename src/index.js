46178579b3ceeb5726e20fd72a212aae815d0ab5
486xtm <gingrasgianat9xtxm@hotmail.com>
2025-03-04 17:23:22 +0800

    commands for specific channel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const { Client, GatewayIntentBits, DiscordAPIError } = require("discord.js");
-const {BOT_TOKEN, CLIENT_ID, GUILD_ID} = require("./utils/config");
+const {
+  BOT_TOKEN,
+  CLIENT_ID,
+  GUILD_ID,
+  ROULETTE_CHANNEL_ID,
+  BLACKJACK_CHANNEL_ID,
+  THREECARDPOKER_CHANNEL_ID,
+  BACCARAT_CHANNEL_ID,
+  HOTCOLD_CHANNEL_ID,
+  FLOWERPOKER_CHANNEL_ID,
+  SPECIAL_CHANNEL_ID,
+} = require("./utils/config");
 const { connect } = require("./utils/database");
 const { commandHandlers, deployCommands } = require("./utils/commands");
 const { helpEmbed } = require("./utils/embeds");
 
 // Add global unhandled rejection handler to prevent crashes
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
   if (error instanceof DiscordAPIError && error.code === 40060) {
-    console.log('Interaction already acknowledged error. Ignoring.');
+    console.log("Interaction already acknowledged error. Ignoring.");
   } else {
-    console.error('Unhandled promise rejection:', error);
+    console.error("Unhandled promise rejection:", error);
   }
   // Don't exit the process, just log the error
 });
@@ -24,13 +35,12 @@ const client = new Client({
 
 // Deploy commands
 
-
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
     activities: [{ name: "/help", type: 0 }],
     status: "online",
-  }); 
+  });
   // const guild = await client.guilds.fetch(GUILD_ID);
   // const existingEmojis = await guild.emojis.fetch();
   // let data = {}
@@ -40,6 +50,15 @@ client.once("ready", async () => {
   // console.log(data);
 });
 
+const gameChannels = {
+  roulette: ROULETTE_CHANNEL_ID,
+  blackjack: BLACKJACK_CHANNEL_ID,
+  threecardpoker: THREECARDPOKER_CHANNEL_ID,
+  baccarat: BACCARAT_CHANNEL_ID,
+  hotcold: HOTCOLD_CHANNEL_ID,
+  flowerpoker: FLOWERPOKER_CHANNEL_ID,
+};
+
 client.on("interactionCreate", async (interaction) => {
   try {
     if (!interaction.isChatInputCommand()) return;
@@ -47,6 +66,20 @@ client.on("interactionCreate", async (interaction) => {
     const { commandName } = interaction;
 
     if (commandHandlers[commandName]) {
+      if (interaction.channelId === SPECIAL_CHANNEL_ID) {
+        await commandHandlers[commandName](interaction);
+        return;
+      }
+      if (
+        gameChannels[commandName] &&
+        interaction.channelId !== gameChannels[commandName]
+      ) {
+        await interaction.reply({
+          content: `You can only play ${commandName} in the #${commandName} channel!`,
+          ephemeral: true,
+        });
+        return;
+      }
       await commandHandlers[commandName](interaction);
       return;
     }
@@ -63,8 +96,8 @@ client.on("interactionCreate", async (interaction) => {
         await interaction.reply(helpEmbed);
         break;
     }
-  } catch(err) {
-    console.log("unknown error", err)
+  } catch (err) {
+    console.log("unknown error", err);
   }
 });
 connect();
